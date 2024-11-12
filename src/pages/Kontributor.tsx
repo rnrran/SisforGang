b4355c00854daf09c9f89b5ade1f3d684f332517
mknayam16 <zdenzel7@gmail.com>
2024-11-12 12:07:46 +0800

    update page Kontributor

--- src/pages/Kontributor.tsx
+++ src/pages/Kontributor.tsx
@@ -1,21 +1,19 @@
-import MessageListItem from '../components/MessageListItem';
 import { useState } from 'react';
 import { Message, getMessages } from '../data/messages';
-import '../components/Navbar/Header'
+import '../components/Navbar/Header';
 import {
   IonContent,
-  IonHeader,
-  IonList,
+  IonGrid,
+  IonRow,
+  IonCol,
   IonPage,
-  IonRefresher,
-  IonRefresherContent,
-  IonTitle,
   IonText,
-  IonToolbar,
   useIonViewWillEnter
 } from '@ionic/react';
 import './Home.css';
 import Header from '../components/Navbar/Header';
+import { logoGithub } from 'ionicons/icons';
+import { IonIcon } from '@ionic/react';
 
 const Kontributor: React.FC = () => {
 
@@ -26,23 +24,43 @@ const Kontributor: React.FC = () => {
     setMessages(msgs);
   });
 
-  const refresh = (e: CustomEvent) => {
-    setTimeout(() => {
-      e.detail.complete();
-    }, 3000);
-  };
+  const teamMembers = [
+    { name: 'Randy Kamal Husein', github: 'https://github.com/rnrran' },
+    { name: 'Lizza Mirizza', github: 'https://github.com/Lizzamirizza' },
+    { name: 'Denzel Samuel Noah Simatupang', github: 'https://github.com/mknayam16' },
+    { name: 'Meisa Maharti', github: 'https://github.com/xxmeii' },
+    { name: 'Dhiaz Wirayuda', github: 'https://github.com/Dayez000' }
+  ];
 
   return (
     <IonPage id="home-page">
-      
       <Header/>
-
       <IonContent fullscreen>
-            <IonText color="primary">
-                <h1>H1: The quick brown fox jumps over the lazy dog</h1>
-            </IonText>
-      </IonContent>
+        <IonText color="primary" className="team-title">
+          <h1>Our Team</h1>
+        </IonText>
+        
+        <IonGrid>
+          {teamMembers.map((member, index) => (
+            <IonRow key={index} className="team-member-row">
+              <IonCol size="12">
+                <IonText color="primary">
+                  <h2>{member.name}</h2>
+                </IonText>
+                <IonRow className="social-links">
+                  <IonCol>
+                    <a href={member.github} target="_blank" rel="noopener noreferrer">
+                      <IonIcon icon={logoGithub} size="large" />
+                    </a>
+                  </IonCol>
+                  {/* Additional social media icons and links can go here */}
+                </IonRow>
+              </IonCol>
+            </IonRow>
+          ))}
+        </IonGrid>
 
+      </IonContent>
     </IonPage>
   );
 };
